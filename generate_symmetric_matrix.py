6009bc88389b9296b3b1770100f77f1789dc4dcb
FarajLagum <faraj.lagum@gmail.com>
2023-10-13 11:45:54 -0400

    add function to generate symmetric matrix of any size

---
+++ generate_symmetric_matrix.py
@@ -0,0 +1,34 @@
+import random
+# import numpy as np
+
+# def generate_symmetric_matrix(size):
+#     np.random.seed(0)  # a random seed
+#     random_matrix = np.random.randint(1, 16, size=(size, size))
+#     symmetric_matrix = (random_matrix + random_matrix.T) / 2
+#     np.fill_diagonal(symmetric_matrix, 0.0)
+#     return symmetric_matrix
+
+
+random.seed(0)
+
+
+def generate_symmetric_matrix(size):
+
+    symmetric_matrix = [[0] * size for _ in range(size)]
+
+    for i in range(size):
+        for j in range(i, size):
+            if i == j:
+                symmetric_matrix[i][j] = 0
+            else:
+                random_value = random.randint(1, 16)
+                symmetric_matrix[i][j] = random_value
+                symmetric_matrix[j][i] = random_value
+
+    return symmetric_matrix
+
+
+if __name__ == "__main__":
+    matrix_size = 12
+    symmetric_matrix = generate_symmetric_matrix(matrix_size)
+    print(list(symmetric_matrix))
